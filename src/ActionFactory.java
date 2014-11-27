291b87c0cd5b9872a7d03e114d77a1daa8b7f77d
serraventura <serraventura@gmail.com>
2014-11-27 23:45:47 +0000

    rename regex var

--- src/ActionFactory.java
+++ src/ActionFactory.java
@@ -9,30 +9,27 @@ public class ActionFactory {
 	
 	private static final String SUFIX = "Ctrl";
 	private static final String PREFIX = "Controller.";
-	private Pattern regExAllPattern = Pattern.compile("/([A-Za-z])\\w+");
-    private Pattern regExIdPattern = Pattern.compile("/([0-9])");
+	private Pattern regClassPattern = Pattern.compile("/([A-Za-z])\\w+");
+	private Pattern regParamIdPattern = Pattern.compile("/([0-9])");
 
     private Integer id;
     private String action;
     
 	public IAction create(HttpServletRequest request) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
 
-		// regex parse pathInfo
 		Matcher matcher;
 		
-		matcher = regExIdPattern.matcher(request.getPathInfo());
+		matcher = regParamIdPattern.matcher(request.getPathInfo());
 
 		if (matcher.find()) {
 			id = Integer.parseInt(matcher.group(1));
 		}
 
-		matcher = regExAllPattern.matcher(request.getPathInfo());
+		matcher = regClassPattern.matcher(request.getPathInfo());
 		
 		if (matcher.find()){
 			action = matcher.group();
 		}
-		
-		//String action = request.getParameter("a").toString().toLowerCase();
 
 		action = action.replace("/", "").toLowerCase();
 		action = PREFIX+Character.toString(action.charAt(0)).toUpperCase()+action.substring(1)+SUFIX.toString();
